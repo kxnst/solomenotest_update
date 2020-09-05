60ba0501be00b178673a256d19585c21fc3260be
kxnst <46329483+kxnst@users.noreply.github.com>
2020-09-05 13:52:41 +0300

    Add files via upload

--- TreeNode.php
+++ TreeNode.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -29,4 +30,32 @@ class TreeNode
     {
         return $this->children;
     }
+=======
+<?php
+
+declare(strict_types=1);
+class TreeNode
+{
+    public $parent;
+    private $children = null;
+    private $_id;
+    public function __construct($parent, $id)
+    {
+        $this->_id = $id;
+        $this->parent = $parent;
+    }
+    public function setChildren( $children){
+        $this->children[]=$children;
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+>>>>>>> ba02eda... changes
 }
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 //Скрипт створений Костянтином Стадником
 //https://github.com/kxnst
@@ -46,3 +47,60 @@ function printNode($node,$shift=0){
 
 }
 echo 'Время выполнения скрипта: '.round(microtime(true) - $start, 4).' сек.';
+=======
+<?php
+//Скрипт створений Костянтином Стадником
+//https://github.com/kxnst
+//
+
+require_once "TreeNode.php";
+
+
+$start = microtime(true);
+
+$connection = new mysqli("localhost", "root", "", "solomono");
+$query = $connection->query("SELECT * FROM `categories` ORDER BY categories_id");
+$array = [];
+while ($tmp = ($query->fetch_object())) {
+    if ($tmp->parent_id == 0) {
+        $res = new TreeNode(null, $tmp->categories_id);
+        $array[] = $res;
+    } else {
+        $res = new TreeNode($array[$tmp->parent_id], $tmp->categories_id);
+        $parent = $array[$tmp->parent_id - 1];
+        $parent->setChildren($res);
+        $array[] = $res;
+    }
+}
+
+$count = count($array);
+for($i = 0;$i<$count;$i++){
+    if($array[$i]->parent!=null)
+        unset($array[$i]);
+}
+foreach ($array as $node)
+    printNode($node);
+
+
+function printNode($node,$shift=0){
+    if(!is_null($node->getChildren())){
+        for($i = 0; $i<$shift;$i++){
+            echo "-";
+        }
+        echo $node->getId()."\r\n";
+        foreach ($node->getChildren() as $child) {
+            printNode($child,($shift+1));
+        }
+    }
+    elseif (is_null($node->getId())){
+        return;
+    }
+    else{
+        for($i = 0; $i<$shift;$i++){
+            echo "-";
+        }
+        echo $node->getId()."\r\n";
+    }
+
+}
+>>>>>>> ba02eda... changes
